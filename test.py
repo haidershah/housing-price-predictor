9d5f71ccfc152055510eb03a3fb04ff0f562674a
Rameez Haider Shah <rameez@zillowgroup.com>
2018-07-08 13:25:18 -0700

    Improve data fetching

--- test.py
+++ test.py
@@ -1,82 +1,102 @@
 from datetime import date
 from dateutil.parser import parse
 from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
 import math
 
-def get_full_address(number, street, city, state):
-	return number + ' ' + street + ', ' + city + ', ' + state
+def does_unit_exist(unit):
+	return isinstance(unit, str) and unit
 
-def get_house_info(address, zipcode):
+def get_full_address(number, street, unit, city, state):
+	if does_unit_exist(unit):
+		return number + ' ' + street + ' ' + unit + ', ' + city + ', ' + state
+	else:
+		return number + ' ' + street + ', ' + city + ', ' + state
+
+def get_house_info(address, unit, zipcode):
+	house_info = []
 	try:
-		deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
-		result = GetDeepSearchResults(deep_search_response)
+		# if unit value exists, don't make zillow api call
+		if does_unit_exist(unit):
+			house_info.extend(('', address, '', '', '', '', '', '', '', '', ''))
+		else:
+			deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
+			result = GetDeepSearchResults(deep_search_response)
 
-		zillow_id = result.zillow_id
-		home_type = result.home_type
-		home_detail_link = result.home_detail_link
-		graph_data_link = result.graph_data_link
-		year_built = result.year_built
-		sqft = result.home_size
-		lot = result.property_size
-		bed = result.bedrooms
-		bath = result.bathrooms
-		last_sold_date = result.last_sold_date
-		last_sold_price = result.last_sold_price
+			zillow_id = result.zillow_id
+			home_type = result.home_type
+			home_detail_link = result.home_detail_link
+			graph_data_link = result.graph_data_link
+			year_built = result.year_built
+			sqft = result.home_size
+			lot = result.property_size
+			bed = result.bedrooms
+			bath = result.bathrooms
+			last_sold_date = result.last_sold_date
+			last_sold_price = result.last_sold_price
 
-		# for properties with missing info, don't add them to training data
-		if (sqft is None) or (int(bed) == 0) or (lot is None) or (year_built is None) or (last_sold_date is None) or (last_sold_price is None):
-			return []
-
-		house_info = []
-		house_info.extend((zillow_id, address, zipcode, sqft, lot, bed, bath,
-							home_type, year_built, last_sold_date, last_sold_price))
-		housing_data_list.append(house_info)
+			house_info.extend((zillow_id, address, zipcode, sqft, lot, bed, bath,
+								home_type, year_built, last_sold_date, last_sold_price))
 	except:
+		print(address + ': exception')
 		return []
 
-zillow_data = ZillowWrapper('X1-ZWz18hg7w22k97_6frtk')
+	return house_info
+
+zillow_data = ZillowWrapper('X1-ZWz18haqkyt2q3_ac8os')
 
 addresses_df = pd.read_csv('data/dublin_addresses.csv')
-addresses_array = np.array(addresses_df[['NUMBER', 'STREET', 'CITY', 'POSTCODE']])
+addresses_array = np.array(addresses_df[['NUMBER', 'STREET', 'UNIT', 'CITY', 'POSTCODE']])
 
-housing_data_df = pd.read_csv('data/dublin_housing_data.csv')
-house_address_array = np.array(housing_data_df['address'])
-house_address_set = set(house_address_array.flat)
+housing_data_file_name = 'data/dublin_housing_data.csv'
+house_address_set = set()
+housing_data_list = []
 
-house_data_array = np.array(housing_data_df[['zillow_id', 'address', 'zipcode', 'sqft', 
+if Path(housing_data_file_name).is_file():
+	print('file exists')
+	housing_data_df = pd.read_csv(housing_data_file_name)
+	house_address_array = np.array(housing_data_df['address'])
+	house_address_set = set(house_address_array.flat)
+
+	house_data_array = np.array(housing_data_df[['zillow_id', 'address', 'zipcode', 'sqft', 
 												'lot', 'bed', 'bath', 'home_type', 'year_built',
 												'last_sold_date', 'last_sold_price']])
+	housing_data_list = house_data_array.tolist()
 
-index = 0
-housing_data_list = house_data_array.tolist()
+num_properties_downloaded = 0
+
+deep_search_response = zillow_data.get_deep_search_results('4855 Swinford ct, dublin, ca', '94568')
+result = GetDeepSearchResults(deep_search_response)
+print (result.home_size)
 
 for address_array in addresses_array:
 
-	index = index + 1
-	print (str(index) + ' properties downloaded.')
-
-	if index == 500:
+	if num_properties_downloaded == 500:
 		break
 
 	number = address_array[0]
 	street = address_array[1]
-	city = address_array[2]
+	unit = address_array[2]
+	city = address_array[3]
 	state = 'CA'
-	zipcode = address_array[3]
+	zipcode = address_array[4]
 
-	address = get_full_address(number, street, city, state)
+	address = get_full_address(number, street, unit, city, state)
 	zipcode = str(int(zipcode))
 
-	#if this property has already been downloaded, skip it
+	# if this property has already been downloaded, skip it
 	if address in house_address_set:
+		print(address + ': already downloaded')
 		continue
 
-	house_info = get_house_info(address, zipcode)
+	house_info = get_house_info(address, unit, zipcode)
 
 	if house_info: # if we have info for this house
+		num_properties_downloaded = num_properties_downloaded + 1
+		print (str(num_properties_downloaded) + ' properties downloaded.')
 		housing_data_list.append(house_info)
 
 columns = ['zillow_id', 'address', 'zipcode', 'sqft', 'lot', 'bed', 'bath', 
